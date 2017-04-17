473ee4ae3a5f4760122275511fe1ec2eb44667c4
Odin Ugedal <odin@ugedal.com>
2017-04-17 14:16:50 +0200

    Update dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,4 +8,8 @@ alias yaourt='yaourt -A'
 if command_exists nvim ; then
     alias vim='nvim'
 fi
+if command_exists nvim-qt ; then
+    alias gvim='nvim-qt'
+fi
 
+alias lego-webapp-test='(cd-gitroot; echo "Running eslint...";node_modules/eslint/bin/eslint.js app --ignore-path .gitignore --quiet)'
