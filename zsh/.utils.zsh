1ca74ca8b666e308790f37d2b02cc2e6b5ddf3ed
Odin Ugedal <odin@ugedal.com>
2019-05-23 19:07:11 +0200

    Update radio Bardufoss stream url

--- zsh/.utils.zsh
+++ zsh/.utils.zsh
@@ -1,8 +1,9 @@
 
 # Radio BDU
-export RADIO_BDU='http://wms.yourstation.com:8000/radiobardufoss320'
+export RADIO_BDU='http://stream.bardufoss.no/radiobardufoss192'
 
 # NRK
+export NRK_P3='http://lyd.nrk.no/nrk_radio_p2_aac_h.m3u'
 export NRK_P3='http://lyd.nrk.no/nrk_radio_p3_aac_h.m3u'
 export NRK_MP3='http://lyd.nrk.no/nrk_radio_mp3_aac_h.m3u'
 export NRK_KLASSISK='http://lyd.nrk.no/nrk_radio_klassisk_aac_h.m3u'
